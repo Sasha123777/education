4d9f74c9bbbed5df4a5817b1f00914374db91d2c
Aleksandra Artamonova <aartamonova@oggettoweb.com>
2024-09-06 11:52:34 +0300

    fix js

--- js/recursion-and-stack/script.js
+++ js/recursion-and-stack/script.js
@@ -1,13 +1,34 @@
 
 function getReverseNumber(n, a = 0) {
+    console.log(n, a);
     if (n === 0) {
         return a;
     }
 
-    return getReverseNumber(Math.trunc(parseInt((n/10).toFixed(1))), 10 * a + n % 10);
+    const isNoValid = n.toString().includes('0');
+
+    if(isNoValid) {
+        return 'число n не должно содержать нулей';
+    }
+    console.log(n, a);
+    return getReverseNumber(Math.trunc(parseInt((n / 10).toFixed(1))), 10 * a + n % 10);
 }
 
 console.log(getReverseNumber(123))
 console.log(getReverseNumber(45))
 console.log(getReverseNumber(1))
 console.log(getReverseNumber(845123))
+console.log(getReverseNumber(1023))
+
+
+/*
+Втрой параметр в функции нужен для того чтобы запоминать результат
+Первый параметр нужен для того чтобы запоминать текущее число, которое обрезается
+если рассмотреть на примере 123
+
+n    a
+123  0
+12   3
+1    32
+0    321
+*/
