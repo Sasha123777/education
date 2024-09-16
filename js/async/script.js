935433ac1a39238a324a6b9e7417d1925bfb8521
Aleksandra Artamonova <aartamonova@oggettoweb.com>
2024-09-16 17:34:59 +0300

    add async

---
+++ js/async/script.js
@@ -0,0 +1,97 @@
+// задача 1
+
+function promiseAll(promiseArray) {
+    const results = [];
+    let count = 0;
+
+    return new Promise((resolve, reject) => {
+        promiseArray.forEach((promise, index) => {
+            promise.then((val) => {
+                results[index] = val;
+                count += 1;
+                
+                if (count === promiseArray.length) {
+                    resolve(results);
+                }
+            })
+            .catch(error => {
+                reject(error)
+            })
+        })
+    });
+}
+
+Promise.all([
+    new Promise(resolve => setTimeout(() => resolve(1), 3000)),
+    new Promise(resolve => setTimeout(() => resolve(2), 2000)),
+    new Promise(resolve => setTimeout(() => resolve(3), 1000))
+]).then(console.log);
+
+promiseAll([
+    new Promise(resolve => setTimeout(() => resolve(5), 3000)),
+    new Promise(resolve => setTimeout(() => resolve(6), 2000)),
+    new Promise(resolve => setTimeout(() => resolve(7), 1000))
+]).then(console.log);
+
+
+//задача 2
+//const id = 0;
+const id = 1;
+const url = `https://swapi.dev/api/films/${id}/`;
+
+function renderCharacterProfile(character) {
+    console.log(character)
+}
+
+function renderErrorMessage(err) {
+    console.log(err)
+}
+
+const getDataFetch = () => {
+    fetch(url)
+        .then(function (response) {
+            return response.json()
+        })
+        .then(function (movie) {
+            const characterUrl = movie.characters[0]
+            return fetch(characterUrl)
+        })
+        .then(function (response) {
+            return response.json()
+        })
+        .then(function (character) {
+            renderCharacterProfile(character)
+        })
+        .catch(function () {
+            renderErrorMessage('Ошибка выполнения запроса')
+        })
+}
+
+const getDataAsync =  async () => {
+    const response = await fetch(url);
+
+    if(response.status !== 200) {
+        renderErrorMessage('Ошибка выполнения запроса');
+        return;
+    }
+    const data =  await response.json();
+
+    if(data.characters.length == 0) {
+        renderErrorMessage('В ответе запроса нет необходимых данных');
+        return;
+    }
+
+    const responseCharacter = await fetch(data.characters[0]);
+
+    if (responseCharacter.status !== 200) {
+        renderErrorMessage('Ошибка выполнения запроса');
+        return;
+    }
+
+    const dataCharacter = await responseCharacter.json();
+
+    renderCharacterProfile(dataCharacter);
+}
+
+getDataFetch();
+getDataAsync();
